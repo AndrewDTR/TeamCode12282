223d18217b0a0b0b068c2fc6751f6cd5afb8a1a3
Draw <your_email@abc.example>
2023-01-06 11:12:20 -0500

    StandardTrackingWheelLocalizer.java non-absurd values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,8 +31,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.688975; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 41; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -20; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 16; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -7; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
